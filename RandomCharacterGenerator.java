b5feb72367e3916d723bd0525e327cd1ac48d1e7
Anıl Şenay <anilsenay3@gmail.com>
2018-11-28 03:22:14 +0300

    import codes as different method

--- RandomCharacterGenerator.java
+++ RandomCharacterGenerator.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 public class RandomCharacterGenerator{
 
 	public static void main(String[] args) throws InterruptedException{
+                Scanner in = new Scanner(System.in);
+                System.out.print("How many characters do you want? : ");
+                int longOfChars = in.nextInt();
+                System.out.println();
+		randomGenerator(longOfChars);
+	}
 
-		Scanner in = new Scanner(System.in);
-		System.out.print("How many characters do you want? : ");
-		int longOfChars = in.nextInt();
-		System.out.println();
+	public static void randomGenerator (int longOfChars) throws InterruptedException{
 		for(int j = 0; j < longOfChars; j++){
 			for(int i = 0; i < 100; i++){
 
