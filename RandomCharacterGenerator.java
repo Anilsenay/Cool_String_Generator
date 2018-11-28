72116b57708b59d7948dce5394eac29f88cadd45
Anıl Şenay <anilsenay3@gmail.com>
2018-11-28 03:45:52 +0300

    added choices by user

--- RandomCharacterGenerator.java
+++ RandomCharacterGenerator.java
@@ -3,13 +3,27 @@ public class RandomCharacterGenerator{
 
 	public static void main(String[] args) throws InterruptedException{
                 Scanner in = new Scanner(System.in);
-                System.out.print("How many characters do you want? : ");
-                int longOfChars = in.nextInt();
-		String str = "CODED BY ANIL ŞENAY";
-                System.out.println();
-		randomGenerator(longOfChars);
-		System.out.println();
-		stringGenerator(str);
+                System.out.print("What would you like to do?\n"
+                + "1- Generate random characters \n"
+                + "2- Generate an input string with a cool way ;D \n"
+                + "Write your choice (1 or 2) : ");
+		int choice = Integer.parseInt(in.nextLine());
+		if(choice == 1)	{
+                	System.out.print("How many characters do you want? : ");
+                	int longOfChars = in.nextInt();
+                	System.out.println();
+			randomGenerator(longOfChars);
+			System.out.println();
+			stringGenerator("CODED BY ANIL ŞENAY");
+		}
+		if(choice == 2) {
+			System.out.print("Which string do you want to print as a cool way? : ");
+			String str = in.nextLine();
+			System.out.println();
+			stringGenerator(str);
+			System.out.println();
+			stringGenerator("CODED BY ANIL ŞENAY");
+		}
 	}
 
 	public static void randomGenerator (int longOfChars) throws InterruptedException{
