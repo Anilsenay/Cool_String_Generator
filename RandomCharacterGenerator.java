96a5c20ec2a099a5d50a938bce3dafec52ca5958
Anıl Şenay <anilsenay3@gmail.com>
2018-11-28 03:08:14 +0300

    Version 0

---
+++ RandomCharacterGenerator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class RandomCharacterGenerator{
+
+	public static void main(String[] args) throws InterruptedException{
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("How many characters do you want? : ");
+		int longOfChars = in.nextInt();
+		System.out.println();
+		for(int j = 0; j < longOfChars; j++){
+			for(int i = 0; i < 100; i++){
+
+				System.out.print((char)(Math.random()*50+49));
+				Thread.sleep(10);
+				if(i < 99){
+				System.out.print("\b");
+				}
+			}
+		}
+		System.out.println();
+	}
+
+}
