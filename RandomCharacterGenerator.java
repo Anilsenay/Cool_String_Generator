31b7388c591be4c710a2e94977aa93dc52503e20
Anıl Şenay <anilsenay3@gmail.com>
2018-11-28 03:34:32 +0300

    added new method for printing author

--- RandomCharacterGenerator.java
+++ RandomCharacterGenerator.java
@@ -5,8 +5,11 @@ public class RandomCharacterGenerator{
                 Scanner in = new Scanner(System.in);
                 System.out.print("How many characters do you want? : ");
                 int longOfChars = in.nextInt();
+		String str = "CODED BY ANIL ŞENAY";
                 System.out.println();
 		randomGenerator(longOfChars);
+		System.out.println();
+		stringGenerator(str);
 	}
 
 	public static void randomGenerator (int longOfChars) throws InterruptedException{
@@ -22,5 +25,21 @@ public class RandomCharacterGenerator{
 		}
 		System.out.println();
 	}
+        public static void stringGenerator (String str) throws InterruptedException{
+                for(int j = 0; j < str.length(); j++){
+                        for(int i = 0; i < 100; i++){
+				if(i<99){
+                                System.out.print((char)(Math.random()*50+49));
+                                Thread.sleep(3);
+                                }
+				if(i < 99){
+                                System.out.print("\b");
+                                }
+				if(i == 99)
+				System.out.print(str.charAt(j));
+                        }
+                }
+                System.out.println();
+        }
 
 }
