9694690b9cddce0dd693c0987cedbac4914df7d1
Anıl Şenay <anilsenay3@gmail.com>
2018-11-30 02:51:14 +0300

    Added loop for main method and program exit choice

--- RandomCharacterGenerator.java
+++ RandomCharacterGenerator.java
@@ -3,36 +3,52 @@ public class RandomCharacterGenerator{
 
 	public static void main(String[] args) throws InterruptedException{
                 Scanner in = new Scanner(System.in);
-                System.out.print("What would you like to do?\n"
-                + "1- Generate random characters \n"
-                + "2- Generate an input string with a cool way ;D \n"
-                + "Write your choice (1 or 2) : ");
-		int choice = Integer.parseInt(in.nextLine());
-		if(choice == 1)	{
-                	System.out.print("How many characters do you want? : ");
-                	int longOfChars = in.nextInt();
-			System.out.print("Select the speed (1 = slow, 2 = normal, 3 = faster) : ");
-			int speed = in.nextInt();
-                	System.out.println();
-			randomGenerator(longOfChars, speed);
-			System.out.println();
-			stringGenerator("CODED BY ANIL ŞENAY");
+		int choice = 1;
+		while(choice != 0){
+                	System.out.print("What would you like to do?\n"
+                	+ "0- Exit from program\n"
+			+ "1- Generate random characters \n"
+                	+ "2- Generate an input string with a cool way ;D \n"
+                	+ "Write your choice (1 or 2 or 0 to exit) : ");
+			choice = Integer.parseInt(in.nextLine());
+			if(choice == 1)	{
+                		System.out.print("How many characters do you want? : ");
+                		int longOfChars = Integer.parseInt(in.nextLine());
+				System.out.print("Select the speed (1 = slow, 2 = normal, 3 = faster) : ");
+				int speed = Integer.parseInt(in.nextLine());
+                		System.out.println();
+				randomGenerator(longOfChars, speed);
+				System.out.println();
+				stringGenerator("CODED BY ANIL ŞENAY");
+				System.out.println();
+			}
+			else if(choice == 2) {
+				System.out.print("Which string do you want to print as a cool way? : ");
+				String str = in.nextLine();
+				System.out.println();
+				stringGenerator(str);
+				System.out.println();
+				stringGenerator("CODED BY ANIL ŞENAY");
+				System.out.println();
+			}
+			else if(choice == 0) {
+				System.out.println("Program ends");
+				System.exit(0);
+			}
+			else {
+				System.out.println();
+				System.out.println("!!! Wrong input, try again !!!");
+				System.out.println();
+			}
 		}
-		if(choice == 2) {
-			System.out.print("Which string do you want to print as a cool way? : ");
-			String str = in.nextLine();
-			System.out.println();
-			stringGenerator(str);
-			System.out.println();
-			stringGenerator("CODED BY ANIL ŞENAY");
-		}
+
 	}
 
 	public static void randomGenerator (int longOfChars, int speed) throws InterruptedException{
 		for(int j = 0; j < longOfChars; j++){
 			for(int i = 0; i < 100; i++){
 
-				System.out.print((char)(Math.random()*50+49));
+				System.out.print((char)(Math.random()*50+40));
 				Thread.sleep((4-speed) * (long)2.5);
 				if(i < 99){
 				System.out.print("\b");
@@ -45,7 +61,7 @@ public class RandomCharacterGenerator{
                 for(int j = 0; j < str.length(); j++){
                         for(int i = 0; i < 100; i++){
 				if(i<99){
-                                System.out.print((char)(Math.random()*50+49));
+                                System.out.print((char)(Math.random()*90+35));
                                 Thread.sleep(2);
                                 }
 				if(i < 99){
