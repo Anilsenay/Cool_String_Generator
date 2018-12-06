0fe8e17a72c6cb505ed5e278cb2a79bf3ac521f6
Anıl Şenay <anilsenay3@gmail.com>
2018-12-07 00:02:27 +0300

    Texts are colorful now

--- RandomCharacterGenerator.java
+++ RandomCharacterGenerator.java
@@ -5,16 +5,16 @@ public class RandomCharacterGenerator{
                 Scanner in = new Scanner(System.in);
 		int choice = 1;
 		while(choice != 0){
-                	System.out.print("What would you like to do?\n"
+                	System.out.print("\u001B[36m"+"What would you like to do?\n"
                 	+ "0- Exit from program\n"
 			+ "1- Generate random characters \n"
                 	+ "2- Generate an input string with a cool way ;D \n"
                 	+ "Write your choice (1 or 2 or 0 to exit) : ");
 			choice = Integer.parseInt(in.nextLine());
 			if(choice == 1)	{
-                		System.out.print("How many characters do you want? : ");
+                		System.out.print("\u001B[35m"+"How many characters do you want? : ");
                 		int longOfChars = Integer.parseInt(in.nextLine());
-				System.out.print("Select the speed (1 = slow, 2 = normal, 3 = faster) : ");
+				System.out.print("\u001B[35m"+"Select the speed (1 = slow, 2 = normal, 3 = faster) : ");
 				int speed = Integer.parseInt(in.nextLine());
                 		System.out.println();
 				randomGenerator(longOfChars, speed);
@@ -23,7 +23,7 @@ public class RandomCharacterGenerator{
 				System.out.println();
 			}
 			else if(choice == 2) {
-				System.out.print("Which string do you want to print as a cool way? : ");
+				System.out.print("\u001B[35m"+"Which string do you want to print as a cool way? : ");
 				String str = in.nextLine();
 				System.out.println();
 				stringGenerator(str);
@@ -32,12 +32,12 @@ public class RandomCharacterGenerator{
 				System.out.println();
 			}
 			else if(choice == 0) {
-				System.out.println("Program ends");
+				System.out.println("\u001B[33m"+"Program ends");
 				System.exit(0);
 			}
 			else {
 				System.out.println();
-				System.out.println("!!! Wrong input, try again !!!");
+				System.out.println("\u001B[31m"+"!!! Wrong input, try again !!!");
 				System.out.println();
 			}
 		}
@@ -48,7 +48,7 @@ public class RandomCharacterGenerator{
 		for(int j = 0; j < longOfChars; j++){
 			for(int i = 0; i < 100; i++){
 
-				System.out.print((char)(Math.random()*50+40));
+				System.out.print("\u001B[32m"+(char)(Math.random()*50+40));
 				Thread.sleep((4-speed) * (long)2.5);
 				if(i < 99){
 				System.out.print("\b");
@@ -61,14 +61,14 @@ public class RandomCharacterGenerator{
                 for(int j = 0; j < str.length(); j++){
                         for(int i = 0; i < 100; i++){
 				if(i<99){
-                                System.out.print((char)(Math.random()*90+35));
+                                System.out.print("\u001B[32m"+(char)(Math.random()*90+35));
                                 Thread.sleep(2);
                                 }
 				if(i < 99){
                                 System.out.print("\b");
                                 }
 				if(i == 99)
-				System.out.print(str.charAt(j));
+				System.out.print("\u001B[32m"+str.charAt(j));
                         }
                 }
                 System.out.println();
